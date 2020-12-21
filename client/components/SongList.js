164a116e60b84353c475fbfd6d02b1e61a7eafb9
koftov <ahronlu@gmail.com>
2020-12-21 09:12:29 +0200

    add query with graphql-tag

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,7 +1,16 @@
 import React from "react";
+import gql from "graphql-tag";
 
 const SongList = () => {
   return <div>SongList</div>;
 };
+
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`;
 
 export default SongList;
